a2a97bfa62d37c1557d8cd673bfe066dd445a0d0
Naoto Mizuno <naotomizuno@preferred.jp>
2022-05-23 11:50:49 +0900

    wip

--- optuna/samplers/_tpe/parzen_estimator.py
+++ optuna/samplers/_tpe/parzen_estimator.py
@@ -6,6 +6,7 @@ from typing import Optional
 from typing import Tuple
 
 import numpy as np
+import scipy.special as special
 
 from optuna import distributions
 from optuna.distributions import BaseDistribution
@@ -35,6 +36,130 @@ class _ParzenEstimatorParameters(
     )
 ):
     pass
+
+
+TRUNCNORM_TAIL_X = 30
+
+
+def _norm_cdf(x):
+    return special.ndtr(x)
+
+
+def _norm_logcdf(x):
+    return special.log_ndtr(x)
+
+
+def _norm_ppf(q):
+    return special.ndtri(q)
+
+
+def _norm_sf(x):
+    return _norm_cdf(-x)
+
+
+def _norm_logsf(x):
+    return _norm_logcdf(-x)
+
+
+def _truncnorm_get_delta_scalar(a, b):
+    if (a > TRUNCNORM_TAIL_X) or (b < -TRUNCNORM_TAIL_X):
+        return 0
+    if a > 0:
+        delta = _norm_sf(a) - _norm_sf(b)
+    else:
+        delta = _norm_cdf(b) - _norm_cdf(a)
+    delta = max(delta, 0)
+    return delta
+
+
+def _truncnorm_ppf_scalar(q, a, b):
+    shp = np.shape(q)
+    q = np.atleast_1d(q)
+    out = np.zeros(np.shape(q))
+    condle0, condge1 = (q <= 0), (q >= 1)
+    if np.any(condle0):
+        out[condle0] = a
+    if np.any(condge1):
+        out[condge1] = b
+    delta = _truncnorm_get_delta_scalar(a, b)
+    cond_inner = ~condle0 & ~condge1
+    if np.any(cond_inner):
+        qinner = q[cond_inner]
+        if delta > 0:
+            if a > 0:
+                sa, sb = _norm_sf(a), _norm_sf(b)
+                np.place(out, cond_inner,
+                         _norm_isf(qinner * sb + sa * (1.0 - qinner)))
+            else:
+                na, nb = _norm_cdf(a), _norm_cdf(b)
+                np.place(out, cond_inner,
+                         _norm_ppf(qinner * nb + na * (1.0 - qinner)))
+        elif np.isinf(b):
+            np.place(out, cond_inner,
+                     -_norm_ilogcdf(np.log1p(-qinner) + _norm_logsf(a)))
+        elif np.isinf(a):
+            np.place(out, cond_inner,
+                     _norm_ilogcdf(np.log(q) + _norm_logcdf(b)))
+        else:
+            if b < 0:
+                # Solve
+                # norm_logcdf(x)
+                #      = norm_logcdf(a) + log1p(q * (expm1(norm_logcdf(b)
+                #                                    - norm_logcdf(a)))
+                #      = nla + log1p(q * expm1(nlb - nla))
+                #      = nlb + log(q) + log1p((1-q) * exp(nla - nlb)/q)
+                def _f_cdf(x, c):
+                    return _norm_logcdf(x) - c
+
+                nla, nlb = _norm_logcdf(a), _norm_logcdf(b)
+                values = nlb + np.log(q[cond_inner])
+                C = np.exp(nla - nlb)
+                if C:
+                    one_minus_q = (1 - q)[cond_inner]
+                    values += np.log1p(one_minus_q * C / q[cond_inner])
+                x = [optimize._zeros_py.brentq(_f_cdf, a, b, args=(c,),
+                                           maxiter=TRUNCNORM_MAX_BRENT_ITERS)
+                     for c in values]
+                np.place(out, cond_inner, x)
+            else:
+                # Solve
+                # norm_logsf(x)
+                #      = norm_logsf(b) + log1p((1-q) * (expm1(norm_logsf(a)
+                #                                       - norm_logsf(b)))
+                #      = slb + log1p((1-q)[cond_inner] * expm1(sla - slb))
+                #      = sla + log(1-q) + log1p(q * np.exp(slb - sla)/(1-q))
+                def _f_sf(x, c):
+                    return _norm_logsf(x) - c
+
+                sla, slb = _norm_logsf(a), _norm_logsf(b)
+                one_minus_q = (1-q)[cond_inner]
+                values = sla + np.log(one_minus_q)
+                C = np.exp(slb - sla)
+                if C:
+                    values += np.log1p(q[cond_inner] * C / one_minus_q)
+                x = [optimize._zeros_py.brentq(_f_sf, a, b, args=(c,),
+                                           maxiter=TRUNCNORM_MAX_BRENT_ITERS)
+                     for c in values]
+                np.place(out, cond_inner, x)
+        out[out < a] = a
+        out[out > b] = b
+    return (out[0] if (shp == ()) else out)
+
+
+def _ppf(q, a, b):
+    if np.isscalar(a) and np.isscalar(b):
+        return _truncnorm_ppf_scalar(q, a, b)
+    a, b = np.atleast_1d(a), np.atleast_1d(b)
+    if a.size == 1 and b.size == 1:
+        return _truncnorm_ppf_scalar(q, a.item(), b.item())
+    
+    out = None
+    it = np.nditer([q, a, b, out], [],
+                   [['readonly'], ['readonly'], ['readonly'],
+                    ['writeonly', 'allocate']])
+    for (_q, _a, _b, _x) in it:
+        _x[...] = _truncnorm_ppf_scalar(_q, _a, _b)
+    return it.operands[3]
 
 
 class _ParzenEstimator:
@@ -114,17 +239,27 @@ class _ParzenEstimator:
                 assert sigmas is not None
 
                 # We sample from truncnorm.
-                samples = np.full(size, fill_value=high + 1.0, dtype=np.float64)
-                while not (low <= samples).all() or not (samples < high).all():
-                    samples = np.where(
-                        (low <= samples) & (samples < high),
-                        samples,
-                        rng.normal(
-                            size=size,
-                            loc=mus[active],
-                            scale=sigmas[active],
-                        ),
-                    )
+                trunc_low = (low - mus[active]) / sigmas[active]
+                trunc_high = (high - mus[active]) / sigmas[active]
+                samples = np.full((), fill_value=high + 1.0, dtype=np.float64)
+                while (samples >= high).any():
+                    a, b = trunc_low, trunc_high
+                    size = (size,)
+                    random_state = rng
+                    out = np.empty(size)
+
+                    it = np.nditer([a, b], flags=['multi_index'], op_flags=[['readonly'], ['readonly']])
+                    while not it.finished:
+                        idx = (it.multi_index[0],)
+                        U = random_state.uniform(low=0, high=1, size=1)
+                        out[idx] = _ppf(U, it[0], it[1])
+                        it.iternext()
+
+                    rvs = out * sigmas[active] + mus[active]
+
+                    samples = np.where(samples < high, samples, rvs)
+                print(samples)
+                print(sum(samples))
             samples_dict[param_name] = samples
         samples_dict = self._transform_from_uniform(samples_dict)
         return samples_dict
