80bf6d7814c9b103c71ad93e9ce6b783da99b497
Naoto Mizuno <naotomizuno@preferred.jp>
2023-06-23 17:08:15 +0900

    WIP

--- optuna/samplers/_tpe/probability_distributions.py
+++ optuna/samplers/_tpe/probability_distributions.py
@@ -1,36 +1,118 @@
+import abc
 from typing import List
 from typing import NamedTuple
-from typing import Union
 
 import numpy as np
 
 from optuna.samplers._tpe import _truncnorm
 
 
-class _BatchedCategoricalDistributions(NamedTuple):
-    weights: np.ndarray
+class _BatchedDistributions(abc.ABC):
+    @abc.abstractmethod
+    def sample(
+        self, rng: np.random.RandomState, batch_size: int, active_indices: np.ndarray
+    ) -> np.ndarray:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def log_pdf(self, x: np.ndarray) -> np.ndarray:
+        raise NotImplementedError
 
 
-class _BatchedTruncNormDistributions(NamedTuple):
-    mu: np.ndarray
-    sigma: np.ndarray
-    low: float  # Currently, low and high do not change per trial.
-    high: float
+class _BatchedCategoricalDistributions(_BatchedDistributions):
+    def __init__(self, weights: np.ndarray) -> None:
+        self._weights = weights
+
+    def sample(
+        self, rng: np.random.RandomState, batch_size: int, active_indices: np.ndarray
+    ) -> np.ndarray:
+        active_weights = self._weights[active_indices, :]
+        rnd_quantile = rng.rand(batch_size)
+        cum_probs = np.cumsum(active_weights, axis=-1)
+        assert np.isclose(cum_probs[:, -1], 1).all()
+        cum_probs[:, -1] = 1  # Avoid numerical errors.
+        return np.sum(cum_probs < rnd_quantile[:, None], axis=-1)
+
+    def log_pdf(self, x: np.ndarray) -> np.ndarray:
+        return np.log(
+            np.take_along_axis(
+                self._weights[None, :, :], x[:, None, None].astype(np.int64), axis=-1
+            )
+        )[:, :, 0]
 
 
-class _BatchedDiscreteTruncNormDistributions(NamedTuple):
-    mu: np.ndarray
-    sigma: np.ndarray
-    low: float  # Currently, low, high and step do not change per trial.
-    high: float
-    step: float
+class _BatchedTruncNormDistributions(_BatchedDistributions):
+    def __init__(self, mu: np.ndarray, sigma: np.ndarray, low: float, high: float) -> None:
+        self._mu = mu
+        self._sigma = sigma
+        self._low = low  # Currently, low and high do not change per trial.
+        self._high = high
+
+    def sample(
+        self, rng: np.random.RandomState, batch_size: int, active_indices: np.ndarray
+    ) -> np.ndarray:
+        active_mus = self._mu[active_indices]
+        active_sigmas = self._sigma[active_indices]
+        return _truncnorm.rvs(
+            a=(self._low - active_mus) / active_sigmas,
+            b=(self._high - active_mus) / active_sigmas,
+            loc=active_mus,
+            scale=active_sigmas,
+            random_state=rng,
+        )
+
+    def log_pdf(self, x: np.ndarray) -> np.ndarray:
+        return _truncnorm.logpdf(
+            x=x[:, None],
+            a=(self._low - self._mu[None, :]) / self._sigma[None, :],
+            b=(self._high - self._mu[None, :]) / self._sigma[None, :],
+            loc=self._mu[None, :],
+            scale=self._sigma[None, :],
+        )
 
 
-_BatchedDistributions = Union[
-    _BatchedCategoricalDistributions,
-    _BatchedTruncNormDistributions,
-    _BatchedDiscreteTruncNormDistributions,
-]
+class _BatchedDiscreteTruncNormDistributions(_BatchedDistributions):
+    def __init__(
+        self, mu: np.ndarray, sigma: np.ndarray, low: float, high: float, step: float
+    ) -> None:
+        self._mu = mu
+        self._sigma = sigma
+        self._low = low  # Currently, low, high and step do not change per trial.
+        self._high = high
+        self._step = step
+
+    def sample(
+        self, rng: np.random.RandomState, batch_size: int, active_indices: np.ndarray
+    ) -> np.ndarray:
+        active_mus = self._mu[active_indices]
+        active_sigmas = self._sigma[active_indices]
+        samples = _truncnorm.rvs(
+            a=(self._low - self._step / 2 - active_mus) / active_sigmas,
+            b=(self._high + self._step / 2 - active_mus) / active_sigmas,
+            loc=active_mus,
+            scale=active_sigmas,
+            random_state=rng,
+        )
+        return np.clip(
+            self._low + np.round((samples - self._low) / self._step) * self._step,
+            self._low,
+            self._high,
+        )
+
+    def log_pdf(self, x: np.ndarray) -> np.ndarray:
+        lower_limit = self._low - self._step / 2
+        upper_limit = self._high + self._step / 2
+        x_lower = np.maximum(x - self._step / 2, lower_limit)
+        x_upper = np.minimum(x + self._step / 2, upper_limit)
+        log_gauss_mass = _truncnorm._log_gauss_mass(
+            (x_lower[:, None] - self._mu[None, :]) / self._sigma[None, :],
+            (x_upper[:, None] - self._mu[None, :]) / self._sigma[None, :],
+        )
+        log_p_accept = _truncnorm._log_gauss_mass(
+            (self._low - self._step / 2 - self._mu[None, :]) / self._sigma[None, :],
+            (self._high + self._step / 2 - self._mu[None, :]) / self._sigma[None, :],
+        )
+        return log_gauss_mass - log_p_accept
 
 
 class _MixtureOfProductDistribution(NamedTuple):
@@ -42,38 +124,7 @@ class _MixtureOfProductDistribution(NamedTuple):
 
         ret = np.empty((batch_size, len(self.distributions)), dtype=np.float64)
         for i, d in enumerate(self.distributions):
-            if isinstance(d, _BatchedCategoricalDistributions):
-                active_weights = d.weights[active_indices, :]
-                rnd_quantile = rng.rand(batch_size)
-                cum_probs = np.cumsum(active_weights, axis=-1)
-                assert np.isclose(cum_probs[:, -1], 1).all()
-                cum_probs[:, -1] = 1  # Avoid numerical errors.
-                ret[:, i] = np.sum(cum_probs < rnd_quantile[:, None], axis=-1)
-            elif isinstance(d, _BatchedTruncNormDistributions):
-                active_mus = d.mu[active_indices]
-                active_sigmas = d.sigma[active_indices]
-                ret[:, i] = _truncnorm.rvs(
-                    a=(d.low - active_mus) / active_sigmas,
-                    b=(d.high - active_mus) / active_sigmas,
-                    loc=active_mus,
-                    scale=active_sigmas,
-                    random_state=rng,
-                )
-            elif isinstance(d, _BatchedDiscreteTruncNormDistributions):
-                active_mus = d.mu[active_indices]
-                active_sigmas = d.sigma[active_indices]
-                samples = _truncnorm.rvs(
-                    a=(d.low - d.step / 2 - active_mus) / active_sigmas,
-                    b=(d.high + d.step / 2 - active_mus) / active_sigmas,
-                    loc=active_mus,
-                    scale=active_sigmas,
-                    random_state=rng,
-                )
-                ret[:, i] = np.clip(
-                    d.low + np.round((samples - d.low) / d.step) * d.step, d.low, d.high
-                )
-            else:
-                assert False
+            ret[:, i] = d.sample(rng, batch_size, active_indices)
 
         return ret
 
@@ -81,38 +132,7 @@ class _MixtureOfProductDistribution(NamedTuple):
         batch_size, n_vars = x.shape
         log_pdfs = np.empty((batch_size, len(self.weights), n_vars), dtype=np.float64)
         for i, d in enumerate(self.distributions):
-            xi = x[:, i]
-            if isinstance(d, _BatchedCategoricalDistributions):
-                log_pdfs[:, :, i] = np.log(
-                    np.take_along_axis(
-                        d.weights[None, :, :], xi[:, None, None].astype(np.int64), axis=-1
-                    )
-                )[:, :, 0]
-            elif isinstance(d, _BatchedTruncNormDistributions):
-                log_pdfs[:, :, i] = _truncnorm.logpdf(
-                    x=xi[:, None],
-                    a=(d.low - d.mu[None, :]) / d.sigma[None, :],
-                    b=(d.high - d.mu[None, :]) / d.sigma[None, :],
-                    loc=d.mu[None, :],
-                    scale=d.sigma[None, :],
-                )
-            elif isinstance(d, _BatchedDiscreteTruncNormDistributions):
-                lower_limit = d.low - d.step / 2
-                upper_limit = d.high + d.step / 2
-                x_lower = np.maximum(xi - d.step / 2, lower_limit)
-                x_upper = np.minimum(xi + d.step / 2, upper_limit)
-                log_gauss_mass = _truncnorm._log_gauss_mass(
-                    (x_lower[:, None] - d.mu[None, :]) / d.sigma[None, :],
-                    (x_upper[:, None] - d.mu[None, :]) / d.sigma[None, :],
-                )
-                log_p_accept = _truncnorm._log_gauss_mass(
-                    (d.low - d.step / 2 - d.mu[None, :]) / d.sigma[None, :],
-                    (d.high + d.step / 2 - d.mu[None, :]) / d.sigma[None, :],
-                )
-                log_pdfs[:, :, i] = log_gauss_mass - log_p_accept
-
-            else:
-                assert False
+            log_pdfs[:, :, i] = d.log_pdf(x[:, i])
         weighted_log_pdf = np.sum(log_pdfs, axis=-1) + np.log(self.weights[None, :])
         max_ = weighted_log_pdf.max(axis=1)
         # We need to avoid (-inf) - (-inf) when the probability is zero.
